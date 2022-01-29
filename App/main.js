dd56e2d698e2f8d4b0339115f615c5cc3742a8fa
Joseph-shumway <joseph.shumway@tamu.edu>
2022-01-29 14:57:32 -0600

    added nodemon for web server

--- App/main.js
+++ App/main.js
@@ -1,2 +1,13 @@
 //backend make request class upload to json file
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

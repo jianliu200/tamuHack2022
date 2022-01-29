6a247f28a9dcbee010509b4136ec3142613aa924
jianliu200 <jiangyuanliu2002@gmail.com>
2022-01-29 15:12:17 -0600

    Added an object in request.js

---
+++ booking website(reference)/request.js
@@ -0,0 +1,22 @@
+hr = window.prompt("What time do you want to go? ")
+fullname = window.prompt("What is the name? ")
+typ = window.prompt("What area do you want to go to? ")
+size = window.prompt("How many people are coming along? ")
+day = window.prompt("When do you want to go? ")
+mail = window.prompt("What is the email? ")
+number = window.prompt("What is the phone number? ")
+
+
+
+
+const request = {
+    name = fullname,
+    date = day,
+    time = hr,
+    type = typ,
+    amount = size,
+    email = mail,
+    phone = number
+
+}
+

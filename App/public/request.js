3d5af5dffd9a183b0b05afb82db92e709a5df697
jianliu200 <jiangyuanliu2002@gmail.com>
2022-01-29 21:52:11 -0600

    fixed request

--- App/public/request.js
+++ App/public/request.js
@@ -1,6 +1,6 @@
 console.log("imported")
 
-const fs = require('fs');
+import { writeFile, appendFile } from 'fs';
 //const { type } = require('os');
 
 class torequest{
@@ -17,8 +17,8 @@ class torequest{
 }
 
 let newrequest = new torequest("10/12/2022", "10:22:05", "MSC", 20, "Sam", "san@gmail.com", "999-999-9999")
-let newr = new torequest("10/20/2022", "10:22", "MSC", 5, "Tom", "Tom@gamil.com", "99-991-9999")
-fs.writeFile("tourSchedule.json", JSON.stringify(newrequest), err => {
+let newr = new torequest("10/20/2022", "10:22", "MSC", 5, "Tom", "Tom@gamil.com", "999-991-9999")
+writeFile("tourSchedule.json", JSON.stringify(newrequest) + '\n', err => {
      
     // Checking for errors
     if (err) throw err; 
@@ -26,7 +26,7 @@ fs.writeFile("tourSchedule.json", JSON.stringify(newrequest), err => {
     console.log("Done writing"); // Success
 });
 
-fs.writeFile("tourSchedule.json", JSON.stringify(newr), err => {
+appendFile("tourSchedule.json", JSON.stringify(newr) + '\n', err => {
      
     // Checking for errors
     if (err) throw err; 

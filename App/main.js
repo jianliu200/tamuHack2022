d03ec37f6b63e69555785004582e5b476ccf9d12
Joseph-shumway <joseph.shumway@tamu.edu>
2022-01-29 22:55:58 -0600

    updated times.html

--- App/main.js
+++ App/main.js
@@ -1,6 +1,10 @@
 //backend make request class upload to json file
 
 const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser')
+let _request = require('./public/request.js');
+
 const app = express()
 const port = 8080
 
@@ -9,6 +13,28 @@ app.set('view engine', 'ejs');
 
 // Where to store frontend assets
 app.use(express.static('public'));
+
+// Allow for req.body to be utilized in '/upvote-joke'
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.post('/submitRequest', (req, res) => {
+    let month = req.body.month
+    let day = req.body.day
+    let time = req.body.time
+
+    let new_request = _request.createRequest(month + day, time, "MSC", 20, "Sam", "san@gmail.com", "999-999-9999")
+
+    _request.writeToJSON("filename.json", new_request)
+    console.log(new_request)
+
+})
+
+
+
+
 
 // landing page will be /views/index.js
 app.get('/', (req, res) => {
--- App/public/request.js
+++ App/public/request.js
@@ -1,9 +1,8 @@
 console.log("imported")
-
 const fs = require('fs');
 //const { type } = require('os');
 
-class torequest{
+class tourRequest{
     constructor(date, time, type, amount, name, email, phone){
         this.person = name;
         this.date = date;
@@ -16,22 +15,38 @@ class torequest{
 
 }
 
-let newrequest = new torequest("10/12/2022", "10:22:05", "MSC", 20, "Sam", "san@gmail.com", "999-999-9999")
-let newr = new torequest("10/20/2022", "10:22", "MSC", 5, "Tom", "Tom@gamil.com", "99-991-9999")
-fs.writeFile("tourSchedule.json", JSON.stringify(newrequest), err => {
+module.exports = {
+
+    writeToJSON: function(filename, request) {
+        fs.writeFile(filename, JSON.stringify(request), err => {
      
-    // Checking for errors
-    if (err) throw err; 
+            // Checking for errors
+            if (err) throw err; 
+        
+            console.log("Done writing"); // Success
 
-    console.log("Done writing"); // Success
-});
+            
+        });
+    },
 
-fs.writeFile("tourSchedule.json", JSON.stringify(newr), err => {
+    createRequest: function(_date, _time, _type, _amount, _name, _email, _phone) {
+        
+        
+        let request = new tourRequest(_date, _time, _type, _amount, _name, _email, _phone)
+        return request
+    }
+
+}
+
+let newrequest = new tourRequest("10/12/2022", "10:22:05", "MSC", 20, "Sam", "san@gmail.com", "999-999-9999")
+let newr = new tourRequest("10/20/2022", "10:22", "MSC", 5, "Tom", "Tom@gamil.com", "99-991-9999")
+
+// fs.writeFile("tourSchedule.json", JSON.stringify(newr), err => {
      
-    // Checking for errors
-    if (err) throw err; 
+//     // Checking for errors
+//     if (err) throw err; 
 
-    console.log("Done writing"); // Success
-});
+//     console.log("Done writing"); // Success
+// });
 
 
